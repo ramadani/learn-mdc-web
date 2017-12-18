b8f636ac5206e02a3f0b969c4d5fe01b326e0111
Ramadani <email.ramadani@gmail.com>
2017-12-18 10:54:20 +0700

    using rmwc

--- src/Hello.js
+++ src/Hello.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'rmwc/Button';
 
 class Hello extends Component {
   constructor(props) {
@@ -18,10 +19,9 @@ class Hello extends Component {
         <div className="mdc-layout-grid__inner">
           <div className="mdc-layout-grid__cell">
             <h1 className="mdc-typography--display1">Hello, {this.state.name}</h1>
-            <button type="button" className="mdc-button mdc-button--raised"
-              onClick={() => this.handlePressMe()}>
+            <Button raised onClick={() => this.handlePressMe()}>
               Press Me
-            </button>
+            </Button>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line
 import './index.css';
+import '../node_modules/material-components-web/dist/material-components-web.css';
 // eslint-disable-next-line
 import App from './App';
 import Hello from './Hello';
