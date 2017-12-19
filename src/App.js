0d2a2c51e0e3399ccdcb5e6fa464aab493205583
Ramadani <email.ramadani@gmail.com>
2017-12-19 17:05:02 +0700

    using drawer

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ import {
   ToolbarMenuIcon
 } from 'rmwc/Toolbar';
 import { Grid } from 'rmwc/Grid';
-// eslint-disable-next-line
+import {
+  TemporaryDrawer,
+  TemporaryDrawerHeader,
+  TemporaryDrawerContent
+} from 'rmwc/Drawer';
+import { ListItem, ListItemText } from 'rmwc/List';
 import { Typography } from 'rmwc/Typography';
-// eslint-disable-next-line
-import { Button } from 'rmwc/Button';
+
 import CardCell from './CardCell';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Nyunmor'
+      title: 'Nyunmor',
+      isDrawerOpen: false,
     }
   }
 
@@ -27,17 +32,43 @@ class App extends Component {
       <CardCell key={number} />
     );
 
+    const navItems = [...Array(7).keys()].map(number => {
+      return (
+        <ListItem ripple>
+          <ListItemText>Menu Item {number}</ListItemText>
+        </ListItem>
+      )
+    });
+
     return (
       <div>
         <Toolbar fixed>
           <ToolbarRow>
             <ToolbarSection alignStart>
-              <ToolbarMenuIcon use="menu" />
+              <ToolbarMenuIcon
+                use="menu"
+                onClick={() =>
+                  this.setState({isDrawerOpen: true})
+                }
+              />
               <ToolbarTitle>{this.state.title}</ToolbarTitle>
             </ToolbarSection>
           </ToolbarRow>
         </Toolbar>
         <ToolbarFixedAdjust />
+
+        <TemporaryDrawer
+          open={this.state.isDrawerOpen}
+          onClose={() => this.setState({isDrawerOpen: false})}
+        >
+          <TemporaryDrawerHeader style={{backgroundColor: '#f5f5f5'}}>
+            <Typography use="subheading2" tag="div">
+              {this.state.title}
+            </Typography>
+          </TemporaryDrawerHeader>
+          <TemporaryDrawerContent>{navItems}</TemporaryDrawerContent>
+        </TemporaryDrawer>
+
         <Grid>{cards}</Grid>
       </div>
     );
